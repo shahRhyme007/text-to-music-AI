5a2a05bd7cc6168c4c2b0df84e354e9e44e3eb9d
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-05-11 17:47:17 -0500

    initial commit

---
+++ audiocraft/app.py
@@ -0,0 +1,33 @@
+from audiocraft.models import MusicGen
+import streamlit as st
+import os
+import torch
+import torchaudio
+import numpy as np
+import base64 # it is used to download the music that is generated
+
+
+# function used to load the model(musicgen-melody is a powerful model)
+
+@st.cache_resource
+def load_model():
+    model = MusicGen.get_pretrained("facebook/musicgen-melody")
+    return model
+
+
+st.set_page_config(page_title="Text to Music Generation",
+                  page_icon=":musical_note:")
+
+
+
+def main(): 
+    # streamlit functionality starting from here
+    st.title("Text to Music Generation")
+
+    with st.expander("See explanation"):
+        st.write("This is a  music generation app built using Meta's Audiocraft Music Gen Model. Based onyour  natural laguage description , it can generate a maximum of 20 seconds music for you. ") 
+        text_area = st.area("Enter your description so that we can create a music for you.....")
+
+
+if __name__ == "__main__": 
+    main()
